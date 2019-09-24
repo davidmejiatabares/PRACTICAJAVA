1aabd7e561a0534577bf6ad8ce02c72a779826b6
davidmejiatabares <55706584+davidmejiatabares@users.noreply.github.com>
2019-09-24 14:39:37 -0500

    Primer Commit

---
+++ EjemploMaven/src/main/java/Practica1/EjemploMaven/App.java
@@ -0,0 +1,13 @@
+package Practica1.EjemploMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
